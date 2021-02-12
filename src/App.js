9a1d567a982b5175ad47d79919861d23fc09d969
shivam <shivampatel27102001@gmail.com>
2021-02-12 17:51:02 +0530

    package lock,json,app.js and home.js file updated

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import React, {useState} from 'react';
 import './App.css';
 import Effect from './components/Effect';
 import State from './components/State';
+import Home from './quiz/Home';
 
 function App() {
-  const [num,setNum] = useState(0);
-  return (
-    <div className="App">
-      <br/>
-      {num}
-      <State/>
-      <Effect/>
-      <button onClick={()=>setNum(num+1)}>Update</button>
+  // const [num,setNum] = useState(0);
+  // return (
+  //   <div className="App">
+  //     <br/>
+  //     {num}
+  //     <State/>
+  //     <Effect/>
+  //     <button onClick={()=>setNum(num+1)}>Update</button>
+  //   </div>
+  //);
+return( 
+  <div className="App">
+    <div className="Header">
+      <Home/>
     </div>
-  );
+  </div>
+);
 }
 
 export default App;
